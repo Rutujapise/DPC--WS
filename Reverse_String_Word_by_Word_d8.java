8dde07cda3f92599b0f9003077f5652b140c5eeb
Rutujapise <166809464+Rutujapise@users.noreply.github.com>
2025-08-22 14:01:52 +0530

    Create Reverse_String_Word_by_Word_d8.java
    day 8 challenge

---
+++ Reverse_String_Word_by_Word_d8.java
@@ -0,0 +1,23 @@
+public class ReverseWords {
+    public static String reverseWords(String s) {
+        s = s.trim();
+  
+        String[] words = s.split("\\s+");
+        
+        //  Reverse words
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("the sky is blue"));   
+        System.out.println(reverseWords("  hello world  "));   
+        System.out.println(reverseWords("a good   example"));  
+        System.out.println(reverseWords("    "));              
+        System.out.println(reverseWords("word"));              
+    }
+}
